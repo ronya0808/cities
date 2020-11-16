066ec8772d86569275253666be97c2c82a77ed58
ronya0808 <74092011+ronya0808@users.noreply.github.com>
2020-11-16 09:49:53 +0700

    Add files via upload

---
+++ src/components/form.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Form = props =>(
+    <form onSubmit={props.weatherMetod}>
+             <input type="text" name="city" placeholder="Город"/>
+            <button>Получить погоду</button>
+    </form>
+ );
+    
+export default Form;
